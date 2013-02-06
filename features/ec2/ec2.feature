547095ee7c17355914077d5fb9a99b6e5135048b
Loren Segal <lsegal@amazon.com>
2013-02-06 01:06:50 -0800

    Move EC2 integration tests into cucumber features

---
+++ features/ec2/ec2.feature
@@ -0,0 +1,29 @@
+# Copyright 2012-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+# language: en
+@ec2
+Feature: Amazon Elastic Compute Cloud
+
+  I want to use Amazon Elastic Compute Cloud
+
+  Scenario: DescribeRegions
+    Given I describe EC2 regions "us-east-1, us-west-1"
+    Then the EC2 endpoint for "us-east-1" should be "ec2.us-east-1.amazonaws.com"
+    And the EC2 endpoint for "us-west-1" should be "ec2.us-west-1.amazonaws.com"
+
+  Scenario: Error handling
+    Given I describe the EC2 instance "i-12345678"
+    Then the error code should be "InvalidInstanceID.NotFound"
+    And the error message should be "The instance ID 'i-12345678' does not exist"
+    And the status code should be 400
---
+++ features/ec2/step_definitions/ec2.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2012-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You
+ * may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+module.exports = function() {
+  this.Before("@ec2", function (callback) {
+    this.client = new this.AWS.EC2.Client();
+    callback();
+  });
+
+  this.Given(/^I describe EC2 regions "([^"]*)"$/, function(regions, callback) {
+    regions = regions.split(/\s*,\s*/)
+    this.request(null, 'describeRegions', {RegionNames: regions}, callback);
+  });
+
+  this.Then(/^the EC2 endpoint for "([^"]*)" should be "([^"]*)"$/, function(region, endpoint, callback) {
+    this.assertions(callback, function(assert) {
+      this.resp.data.Regions.forEach(function(region) {
+        if (region.RegionName === region)
+          assert.equal(region.Endpoint, endpoint);
+      });
+    });
+  });
+
+  this.Given(/^I describe the EC2 instance "([^"]*)"$/, function(instanceId, callback) {
+    this.request(null, 'describeInstances', {InstanceIds: [instanceId]}, callback, false);
+  });
+
+  this.Then(/^the error message should be "([^"]*)"$/, function(message, callback) {
+    this.assertions(callback, function(assert) {
+      assert.equal(this.error.message, message);
+    });
+  });
+};
