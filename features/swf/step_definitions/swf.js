9677901189e3f2dcad1188c5abf33378b46480de
Loren Segal <lsegal@amazon.com>
2014-03-13 15:48:06 -0700

    Gracefully handle SWF limits in integration test

--- features/swf/step_definitions/swf.js
+++ features/swf/step_definitions/swf.js
@@ -7,14 +7,17 @@ module.exports = function() {
   this.Given(/^I register a SWF domain with prefix "([^"]*)"$/, function(prefix, callback) {
     this.swfDomain = prefix + '-' + new Date().getTime();
     var params = {name:this.swfDomain, workflowExecutionRetentionPeriodInDays:'7'};
-    this.request(null, 'registerDomain', params, callback);
+    this.request(null, 'registerDomain', params, callback, false);
   });
 
   this.Given(/^I list SWF domains with status "([^"]*)"$/, function(status, callback) {
+    if (this.error.name === 'LimitExceededFault') return callback();
     this.request(null, 'listDomains', {registrationStatus: status}, callback);
   });
 
   this.Then(/^the SWF domain should be in the list$/, function(callback) {
+    if (this.error.name === 'LimitExceededFault') return callback();
+
     var items = {};
     this.data.domainInfos.forEach(function (info) {
       items[info.name] = info;
@@ -24,6 +27,7 @@ module.exports = function() {
   });
 
   this.Then(/^I delete the SWF domain$/, function(callback) {
+    if (this.error.name === 'LimitExceededFault') return callback();
     this.request(null, 'deprecateDomain', {name: this.swfDomain}, callback);
   });
 
