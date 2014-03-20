3bb39e835ea126d0265266d36fb0d107884cb54d
Loren Segal <lsegal@amazon.com>
2014-03-19 17:07:00 -0700

    Update ESLint no-require rule to no-require-in-service, use filename to scope rule check

--- eslint-rules/no-require-in-service.js
+++ eslint-rules/no-require-in-service.js
@@ -1,6 +1,7 @@
 module.exports = function(context) {
   return {
     CallExpression: function(node) {
+      if (!context.filename.match(/^lib\/services\//)) return;
       if (node.callee.name === 'require' && node.arguments[0].value !== '../core') {
         context.report(node, 'require() is disallowed in service files');
       }
