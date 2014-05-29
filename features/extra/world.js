886e2f9a052559390b5c703f2517ab83f72101f9
Loren Segal <lsegal@amazon.com>
2014-05-29 02:16:55 -0700

    Test new Travis config

--- features/extra/world.js
+++ features/extra/world.js
@@ -1,6 +1,8 @@
 var world = require('./helpers');
 world.AWS = require('../../lib/aws');
-world.AWS.config.loadFromPath(__dirname + '/../../configuration');
+try {
+  world.AWS.config.loadFromPath(__dirname + '/../../configuration');
+} catch (e) { }
 
 var WorldConstructor = function WorldConstructor(callback) {
   callback(world);
