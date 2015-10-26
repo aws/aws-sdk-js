4e8ae64d36e57447a88302f554299e7de57ca21e
chrisradek <radekc@amazon.com>
2015-10-26 12:01:47 -0700

    Tag release v2.2.12

--- dist/aws-sdk.js
+++ dist/aws-sdk.js
@@ -1,4 +1,4 @@
-// AWS SDK for JavaScript v2.2.11
+// AWS SDK for JavaScript v2.2.12
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // License at https://sdk.amazonaws.com/js/BUNDLE_LICENSE.txt
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -295,7 +295,7 @@ module.exports = AWS;
 AWS.util.update(AWS, {
 
 
-  VERSION: '2.2.11',
+  VERSION: '2.2.12',
 
 
   Signers: {},
@@ -1986,9 +1986,11 @@ function translateStructure(structure, shape) {
   if (structure == null) return undefined;
 
   var struct = {};
-  util.each(structure, function(name, value) {
-    var memberShape = shape.members[name];
-    if (memberShape) {
+  var shapeMembers = shape.members;
+  util.each(shapeMembers, function(name, memberShape) {
+    var locationName = memberShape.isLocationName ? memberShape.name : name;
+    if (structure.hasOwnProperty(locationName)) {
+      var value = structure[locationName];
       var result = translate(value, memberShape);
       if (result !== undefined) struct[name] = result;
     }
