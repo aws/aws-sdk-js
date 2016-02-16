b99a4d5e0a12e114027cbcd1402af68ec069d73b
chrisradek <radekc@amazon.com>
2016-02-16 13:59:03 -0800

    Tag release v2.2.36

--- dist/aws-sdk.js
+++ dist/aws-sdk.js
@@ -1,4 +1,4 @@
-// AWS SDK for JavaScript v2.2.35
+// AWS SDK for JavaScript v2.2.36
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // License at https://sdk.amazonaws.com/js/BUNDLE_LICENSE.txt
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -320,7 +320,7 @@ module.exports = AWS;
 AWS.util.update(AWS, {
 
 
-  VERSION: '2.2.35',
+  VERSION: '2.2.36',
 
 
   Signers: {},
