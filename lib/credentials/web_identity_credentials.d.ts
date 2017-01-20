f9f49cb76c33e4108ec7024d9e34e7bfdd6e857c
Christopher Radek <cjradek@gmail.com>
2017-01-20 15:45:44 -0800

    Updates CognitoIdentityCredentials to accept AWS.config options (#1317)
    * Adds clientConfig to CognitoIdentityCredentials to support passing in region and other config to the underlying CognitoIdentity service client
    
    * Passes client config settings to STS clients in credentials

--- lib/credentials/web_identity_credentials.d.ts
+++ lib/credentials/web_identity_credentials.d.ts
@@ -1,12 +1,19 @@
 import {Credentials} from '../credentials';
 import {AWSError} from '../error';
+import {ConfigurationOptions} from '../config';
 import STS = require('../../clients/sts');
 export class WebIdentityCredentials extends Credentials {
     /**
-     * Creates a new credentials object.
-     * @param {string} filename - a map of options that are passed to the AWS.STS.assumeRole() or AWS.STS.getSessionToken() operations. If a RoleArn parameter is passed in, credentials will be based on the IAM role.
+     * Creates a new credentials object with optional configuraion.
+     * @param {Object} options - a map of options that are passed to the AWS.STS.assumeRole() or AWS.STS.getSessionToken() operations. If a RoleArn parameter is passed in, credentials will be based on the IAM role.
+     * @param {Object} clientConfig - a map of configuration options to pass to the underlying STS client.
      */
-    constructor(options?: STS.Types.AssumeRoleWithWebIdentityRequest);
+    constructor(options: WebIdentityCredentials.WebIdentityCredentialsOptions, clientConfig?: ConfigurationOptions);
+    /**
+     * Creates a new credentials object.
+     * @param {string} options - a map of options that are passed to the AWS.STS.assumeRole() or AWS.STS.getSessionToken() operations. If a RoleArn parameter is passed in, credentials will be based on the IAM role.
+     */
+    constructor(options?: WebIdentityCredentials.WebIdentityCredentialsOptions);
     /**
      * Refreshes credentials using AWS.STS.assumeRoleWithWebIdentity().
      */
@@ -14,4 +21,10 @@ export class WebIdentityCredentials extends Credentials {
 
     data: STS.Types.AssumeRoleWithWebIdentityResponse;
     params: STS.Types.AssumeRoleWithWebIdentityRequest
+}
+
+// Needed to expose interfaces on the class
+declare namespace WebIdentityCredentials {
+    export type ClientConfiguration = ConfigurationOptions;
+    export type WebIdentityCredentialsOptions = STS.AssumeRoleWithWebIdentityRequest;
 }
