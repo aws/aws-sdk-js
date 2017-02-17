200370a962b9953b0535226798a574f747db04e3
Jonathan Eskew <eskewj@amazon.com>
2017-02-16 17:14:15 -0800

    Cache derived credentials using an hmac of the AKID signed by the secret key instead of just by AKID

--- test/signers/v4_credentials.js
+++ test/signers/v4_credentials.js
@@ -4,7 +4,10 @@ var helpers = require('../helpers'),
   credentials = {accessKeyId: 'foo', secretAccessKey: 'bar'},
   date = '19700101',
   region = 'us-foo-1',
-  service = 'bar';
+  service = 'bar',
+  maxCacheEntries = 50,
+  hmacsOnCacheHit = 1,
+  hmacsOnCacheMiss = 5;
 
 describe('v4_credentials', function() {
   describe('#createScope', function() {
@@ -45,7 +48,6 @@ describe('v4_credentials', function() {
     });
 
     it('will cache a maximum of 50 derived credentials', function() {
-      var maxCacheEntries = 50;
       for (var i = 0; i < maxCacheEntries; i++) {
         v4Credentials.getSigningKey(
           credentials,
@@ -56,12 +58,14 @@ describe('v4_credentials', function() {
       }
 
       // Deriving a key involves calculating 4 HMAC values
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(maxCacheEntries * 4);
+      expect(AWS.util.crypto.hmac.calls.length).to
+        .equal(maxCacheEntries * hmacsOnCacheMiss);
 
       // Get the signing key that should be in cache
       v4Credentials.getSigningKey(credentials, date, region, service + '_0');
       // AWS.util.crypto.hmac should not have been called again
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(maxCacheEntries * 4);
+      expect(AWS.util.crypto.hmac.calls.length).to
+        .equal(maxCacheEntries * hmacsOnCacheMiss + hmacsOnCacheHit);
 
       // Derive a key that would not be in cache
       v4Credentials.getSigningKey(
@@ -71,56 +75,62 @@ describe('v4_credentials', function() {
         service + '_' + maxCacheEntries.toString(16)
       );
       // AWS.util.crypto.hmac should have been called 4 additional times
-      expect(AWS.util.crypto.hmac.calls.length).to.equal((maxCacheEntries + 1) * 4);
+      expect(AWS.util.crypto.hmac.calls.length).to
+        .equal((maxCacheEntries + 1) * hmacsOnCacheMiss + hmacsOnCacheHit);
 
       // The first signing key should have been evicted from cache
       v4Credentials.getSigningKey(credentials, date, region, service + '_0');
       // AWS.util.crypto.hmac should have been called 4 additional times
-      expect(AWS.util.crypto.hmac.calls.length).to.equal((maxCacheEntries + 2) * 4);
+      expect(AWS.util.crypto.hmac.calls.length).to
+        .equal((maxCacheEntries + 2) * hmacsOnCacheMiss + hmacsOnCacheHit);
     });
 
     it('should cache subsequent requests', function() {
       expect(AWS.util.crypto.hmac.calls.length).to.equal(0);
       v4Credentials.getSigningKey(credentials, date, region, service);
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(4);
+      expect(AWS.util.crypto.hmac.calls.length).to.equal(hmacsOnCacheMiss);
       v4Credentials.getSigningKey(credentials, date, region, service);
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(4);
+      expect(AWS.util.crypto.hmac.calls.length).to
+        .equal(hmacsOnCacheMiss + hmacsOnCacheHit);
       v4Credentials.getSigningKey(credentials, date, region, service);
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(4);
+      expect(AWS.util.crypto.hmac.calls.length).to
+        .equal(hmacsOnCacheMiss + hmacsOnCacheHit * 2);
     });
 
     it('should bypass cache when requested', function() {
       expect(AWS.util.crypto.hmac.calls.length).to.equal(0);
       v4Credentials.getSigningKey(credentials, date, region, service);
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(4);
+      expect(AWS.util.crypto.hmac.calls.length).to.equal(hmacsOnCacheMiss);
       v4Credentials.getSigningKey(credentials, date, region, service);
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(4);
+      expect(AWS.util.crypto.hmac.calls.length).to
+        .equal(hmacsOnCacheMiss + hmacsOnCacheHit);
       v4Credentials.getSigningKey(credentials, date, region, service, false);
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(8);
+      expect(AWS.util.crypto.hmac.calls.length).to
+        .equal(hmacsOnCacheMiss * 2 + hmacsOnCacheHit);
     });
 
     it('should include the provided date in the cache key', function() {
       expect(AWS.util.crypto.hmac.calls.length).to.equal(0);
       v4Credentials.getSigningKey(credentials, date, region, service);
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(4);
+      expect(AWS.util.crypto.hmac.calls.length).to.equal(hmacsOnCacheMiss);
       v4Credentials.getSigningKey(credentials, '20170207', region, service);
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(8);
+      expect(AWS.util.crypto.hmac.calls.length).to.equal(hmacsOnCacheMiss * 2);
     });
 
     it('should include the provided region in the cache key', function() {
       expect(AWS.util.crypto.hmac.calls.length).to.equal(0);
       v4Credentials.getSigningKey(credentials, date, region, service);
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(4);
+      expect(AWS.util.crypto.hmac.calls.length).to.equal(hmacsOnCacheMiss);
       v4Credentials.getSigningKey(credentials, date, region + 'foo', service);
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(8);
+      expect(AWS.util.crypto.hmac.calls.length).to.equal(hmacsOnCacheMiss * 2);
     });
 
     it('should include the provided service in the cache key', function() {
       expect(AWS.util.crypto.hmac.calls.length).to.equal(0);
       v4Credentials.getSigningKey(credentials, date, region, service);
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(4);
+      expect(AWS.util.crypto.hmac.calls.length).to.equal(hmacsOnCacheMiss);
       v4Credentials.getSigningKey(credentials, date, region, service + 'foo');
-      expect(AWS.util.crypto.hmac.calls.length).to.equal(8);
+      expect(AWS.util.crypto.hmac.calls.length).to.equal(hmacsOnCacheMiss * 2);
     });
   });
 });
