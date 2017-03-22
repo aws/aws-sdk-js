0c492450cfb518cfa9e66f9e296f505d647aafae
Jonathan Eskew <eskewj@amazon.com>
2017-03-22 16:33:03 -0700

    Add a means of specifying tags for s3.upload that applies to both single part and multipart uploads

--- lib/s3/managed_upload.d.ts
+++ lib/s3/managed_upload.d.ts
@@ -19,7 +19,7 @@ export class ManagedUpload {
     send(callback?: (err: AWSError, data: ManagedUpload.SendData) => void): void;
     /**
      * Adds a listener that is triggered when theuploader has uploaded more data.
-     * 
+     *
      * @param {string} event - httpUploadProgress: triggered when the uploader has uploaded more data.
      * @param {function} listener - Callback to run when the uploader has uploaded more data.
      */
@@ -44,45 +44,49 @@ export namespace ManagedUpload {
         /**
          * URL of the uploaded object.
          */
-        Location: string
+        Location: string;
         /**
          * ETag of the uploaded object.
          */
-        ETag: string
+        ETag: string;
         /**
          * Bucket to which the object was uploaded.
          */
-        Bucket: string
+        Bucket: string;
         /**
          * Key to which the object was uploaded.
          */
-        Key: string
+        Key: string;
     }
     export interface ManagedUploadOptions {
         /**
          * A map of parameters to pass to the upload requests.
          * The "Body" parameter is required to be specified either on the service or in the params option.
          */
-        params?: S3.Types.PutObjectRequest
+        params?: S3.Types.PutObjectRequest;
         /**
          * The size of the concurrent queue manager to upload parts in parallel. Set to 1 for synchronous uploading of parts. Note that the uploader will buffer at most queueSize * partSize bytes into memory at any given time.
          * default: 4
          */
-        queueSize?: number
+        queueSize?: number;
         /**
          * Default: 5 mb
          * The size in bytes for each individual part to be uploaded. Adjust the part size to ensure the number of parts does not exceed maxTotalParts. See minPartSize for the minimum allowed part size.
          */
-        partSize?: number
+        partSize?: number;
         /**
          * Default: false
          * Whether to abort the multipart upload if an error occurs. Set to true if you want to handle failures manually.
          */
-        leavePartsOnError?: boolean
+        leavePartsOnError?: boolean;
         /**
          * An optional S3 service object to use for requests.
          * This object might have bound parameters used by the uploader.
          */
-        service?: S3
+        service?: S3;
+        /**
+         * The tags to apply to the object.
+         */
+        tags?: Array<S3.Tag>;
     }
 }
