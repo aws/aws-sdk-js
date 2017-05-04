cdd091dc7a27b56a790dcbe4c4f6ff1b5b36ae1e
Jonathan Eskew <eskewj@amazon.com>
2017-05-04 10:56:01 -0700

    Add a TS test for credentials and add promise methods to type definition

--- lib/credentials.d.ts
+++ lib/credentials.d.ts
@@ -2,13 +2,13 @@ import {AWSError} from './error';
 export class Credentials {
     /**
      * Creates a Credentials object with a given set of credential information as an options hash.
-     * 
+     *
      * @param {object} options - An option hash containing a set of credential information.
      */
     constructor(options: CredentialsOptions);
     /**
      * Creates a Credentials object with a given set of credential information as positional arguments.
-     *          * 
+     *          *
      * @param {string} accessKeyId - The AWS access key ID.
      * @param {string} secretAccessKey - The AWS secret access key.
      * @param {string} sessionToken - The optional AWS session token.
@@ -18,10 +18,16 @@ export class Credentials {
      * Gets the existing credentials, refreshing them if they are not yet loaded or have expired.
      * Users should call this method before using refresh(), as this will not attempt to reload
      * credentials when they are already loaded into the object.
-     * 
+     *
      * @param {get} callback - Called when the instance metadata service responds. When called with no error, the credentials information has been loaded into the object.
      */
     get(callback: (err: AWSError) => void): void;
+    /**
+     * Gets the existing credentials, refreshing them if necessary, and returns
+     * a promise that will be fulfilled immediately (if no refresh is necessary)
+     * or when the refresh has completed.
+     */
+    getPromise(): Promise<void>;
     /**
      * Returns whether the credentials object should call refresh()
      */
@@ -29,10 +35,16 @@ export class Credentials {
     /**
      * Refreshes the credentials.
      * Users should call get() before attempting to forcibly refresh credentials.
-     * 
+     *
      * @param {function} callback - Called when the instance metadata service responds. When called with no error, the credentials information has been loaded into the object.
      */
     refresh(callback: (err: AWSError) => void): void;
+    /**
+     * Invokes a credential refresh and returns a promise that will be fulfilled
+     * when the refresh has completed or rejected when the refresh has failed.
+     * Users should call get() before attempting to forcibly refresh credentials.
+     */
+    refreshPromise(): Promise<void>;
     /**
      * AWS access key ID.
      */
