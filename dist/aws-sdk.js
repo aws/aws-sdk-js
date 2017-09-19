f9ade4e72a639fbc722183f45f54005d30aec3d5
awstools <awstools@amazon.com>
2017-09-19 23:46:35 +0000

    Updates SDK to v2.119.0

--- dist/aws-sdk.js
+++ dist/aws-sdk.js
@@ -1,4 +1,4 @@
-// AWS SDK for JavaScript v2.118.0
+// AWS SDK for JavaScript v2.119.0
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // License at https://sdk.amazonaws.com/js/BUNDLE_LICENSE.txt
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -28875,6 +28875,7 @@ module.exports={
             "locationName": "elasticGpuSet",
             "type": "list",
             "member": {
+              "locationName": "item",
               "type": "structure",
               "members": {
                 "ElasticGpuId": {
@@ -107416,7 +107417,7 @@ module.exports = AWS;
 AWS.util.update(AWS, {
 
 
-  VERSION: '2.118.0',
+  VERSION: '2.119.0',
 
 
   Signers: {},
