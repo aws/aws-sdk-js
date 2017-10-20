14e7742c552b43920194962421510d93825be971
zheallan <zyp19930608@gmail.com>
2017-10-20 16:18:53 -0700

    clean integration test for cloudformation & dataPipeline & cloudsearch

--- features/cloudsearch/step_definitions/cloudsearch.js
+++ features/cloudsearch/step_definitions/cloudsearch.js
@@ -8,20 +8,4 @@ module.exports = function() {
     this.domainName = this.uniqueName(prefix);
     this.request(null, 'createDomain', {DomainName: this.domainName}, callback, false);
   });
-
-  this.Given(/^I define a CloudSearch index field$/, function(callback) {
-    var params = {
-      DomainName: this.domainName,
-      IndexField: {
-        IndexFieldName: 'fieldname',
-        IndexFieldType: 'text'
-      }
-    };
-    this.request(null, 'defineIndexField', params, callback);
-  });
-
-  this.Then(/^DomainStatus should show that Created is true$/, function(callback) {
-    this.assert.equal(this.data.DomainStatus.Created, true);
-    callback();
-  });
 };
