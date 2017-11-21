f30c5bfc1b74ca5366f3d4261ae61f3806f9b991
Steve Harris <harrisgj@amazon.com>
2017-11-21 22:33:13 +0000

    Fix export ECSCredentials Travis CI error, try 2

--- lib/credentials/remote_credentials.d.ts
+++ lib/credentials/remote_credentials.d.ts
@@ -15,4 +15,19 @@ export class RemoteCredentials extends Credentials {
         }
         maxRetries?: number
     }
-export class ECSCredentials extends RemoteCredentials
+export class ECSCredentials extends RemoteCredentials {
+        /**
+         * Represents credentials received.
+         * @param {object} options - Override the default (1s) timeout period.
+         */
+        constructor(options?: ECSCredentialsOptions);
+    }
+    interface ECSCredentialsOptions {
+        httpOptions?: {
+            /**
+             * Timeout in milliseconds.
+             */
+            timeout?: number
+        }
+        maxRetries?: number
+    }
