10d611fa3551d132152e27d77baf0880f74fc31e
Christopher Radek <cjradek@gmail.com>
2018-06-15 06:05:46 -0700

    Throws error on truncated event messages and forwards error from upstream streams to destination event stream

--- test/event-stream/mock-event-message-source-stream.fixture.js
+++ test/event-stream/mock-event-message-source-stream.fixture.js
@@ -5,20 +5,29 @@ var Readable = require('stream').Readable;
  * @param {number} emitSize
  * @param {*} options
  */
-function MockEventMessageSource(messages, emitSize, options) {
+function MockEventMessageSource(messages, emitSize, throwError, options) {
     Readable.call(this, options);
 
     this.data = Buffer.concat(messages);
     this.readCount = 0;
     this.emitSize = emitSize;
+    this.throwError = throwError;
 }
 
 MockEventMessageSource.prototype = Object.create(Readable.prototype);
 
 MockEventMessageSource.prototype._read = function() {
+    var self = this;
     if (this.readCount === this.data.length) {
-        this.push(null);
-        return;
+        if (this.throwError) {
+            process.nextTick(function() {
+                self.emit('error', new Error('Throwing an error!'));
+            });
+            return;
+        } else {
+            this.push(null);
+            return;
+        }
     }
 
     var bytesLeft = this.data.length - this.readCount;
