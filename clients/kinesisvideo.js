09ef72478a84b76b89cc03a8dae0777b95eca025
zheallan <zheallan@amazon.com>
2018-09-11 13:50:30 -0700

    clean up files

--- clients/kinesisvideo.js
+++ clients/kinesisvideo.js
@@ -4,12 +4,12 @@ var Service = AWS.Service;
 var apiLoader = AWS.apiLoader;
 
 apiLoader.services['kinesisvideo'] = {};
-AWS.KinesisVideo = Service.defineService('kinesisvideo', ['2018-07-24']);
-Object.defineProperty(apiLoader.services['kinesisvideo'], '2018-07-24', {
+AWS.KinesisVideo = Service.defineService('kinesisvideo', ['2017-09-30']);
+Object.defineProperty(apiLoader.services['kinesisvideo'], '2017-09-30', {
   get: function get() {
-    var model = require('../apis/kinesisvideo-2018-07-24.min.json');
+    var model = require('../apis/kinesisvideo-2017-09-30.min.json');
     model.paginators = require('../apis/kinesisvideo-2017-09-30.paginators.json').pagination;
-    return model;s
+    return model;
   },
   enumerable: true,
   configurable: true
