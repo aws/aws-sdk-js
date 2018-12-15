5f4a8ab03955fea0c33afeaffb53734990816158
Marc Palm <marc.palm@testless.io>
2018-12-15 11:19:33 +0100

    Correct typescript typings accoirding to docs
    https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#batchWrite-property

--- clients/dynamodb.d.ts
+++ clients/dynamodb.d.ts
@@ -961,7 +961,7 @@ declare namespace DynamoDB {
     /**
      * A map of attribute name to attribute values, representing the primary key of the item to delete. All of the table's primary key attributes must be specified, and their data types must match those of the table's key schema.
      */
-    Key: Key;
+    Key: { [key: string]: any };
   }
   export interface DeleteTableInput {
     /**
@@ -1691,7 +1691,7 @@ declare namespace DynamoDB {
     /**
      * A map of attribute name to attribute values, representing the primary key of an item to be processed by PutItem. All of the table's primary key attributes must be specified, and their data types must match those of the table's key schema. If any attributes are present in the item which are part of an index key schema for the table, their types must match the index key schema.
      */
-    Item: PutItemInputAttributeMap;
+    Item: { [key: string]: any };
   }
   export interface QueryInput {
     /**
