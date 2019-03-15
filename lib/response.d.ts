c652be36a664b82e893fca715003e25824018b93
Michael S. Fischer <fiscmi@amazon.com>
2019-03-15 14:56:20 -0700

    AWS.Response.nextPage(): Don't require callback
    AWS.Response.nextPage() does not require a callback argument, so don't
    force the user to specify one when when writing TypeScript code.

--- lib/response.d.ts
+++ lib/response.d.ts
@@ -8,7 +8,7 @@ export class Response<D, E> {
 		/**
 		 * Creates a new request for the next page of response data, calling the callback with the page data if a callback is provided.
 		 */
-		nextPage(callback: (err: E, data: D) => void): Request<D, E>|void;
+		nextPage(callback?: (err: E, data: D) => void): Request<D, E>|void;
 		/**
 		 * The de-serialized response data from the service.
 		 * Can be null if an error occurred.
