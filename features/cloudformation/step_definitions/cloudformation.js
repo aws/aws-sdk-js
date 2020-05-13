b5d439ce74cf2049cee9ed8ed538ce82c9f70963
AllanFly120 <zheallan@amazon.com>
2020-05-13 09:05:43 -0700

    enable termivation protection to cloudformation integ test (#3252)

--- features/cloudformation/step_definitions/cloudformation.js
+++ features/cloudformation/step_definitions/cloudformation.js
@@ -7,7 +7,7 @@ module.exports = function() {
   this.Given(/^I create a CloudFormation stack with name prefix "([^"]*)"$/, function(prefix, callback) {
     this.stackName = this.uniqueName(prefix);
     this.templateBody = '{"Resources":{"member":{"Type":"AWS::SQS::Queue"}}}';
-    var params = { TemplateBody: this.templateBody, StackName: this.stackName };
+    var params = { TemplateBody: this.templateBody, StackName: this.stackName, EnableTerminationProtection: true };
     this.request(null, 'createStack', params, callback, false);
   });
 };
