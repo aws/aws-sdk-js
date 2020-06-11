a5160e579fd6d08f35abd90d982b381ace13e5a7
Florian Morello <arsonik@me.com>
2020-06-11 15:45:16 +0200

    DTypo

--- lib/config.d.ts
+++ lib/config.d.ts
@@ -13,7 +13,7 @@ export class ConfigBase extends ConfigurationOptions{
     getCredentials(callback: (err: AWSError) => void): void;
     /**
      * Loads configuration data from a JSON file into this config object.
-     * Loading configuration willr eset all existing configuration on the object.
+     * Loading configuration will reset all existing configuration on the object.
      * This feature is not supported in the browser environment of the SDK.
      *
      * @param {string} path - the path relative to your process's current working directory to load configuration from.
