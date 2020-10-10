b5cf0eb76ab75ab01990c18abd9b282bdc63af71
Juan Lulkin <juan.lulkin@klarna.com>
2020-10-10 17:22:10 +0200

    Name functions normally, since defineProperty doesn't work on node < 4

--- test/node_loader.spec.js
+++ test/node_loader.spec.js
@@ -4,16 +4,17 @@ const AWS = require('../lib/node_loader');
 describe('node_loader', () => {
   describe('AWS.CredentialProviderChain', () => {
     it('initiates providers with named functions', () => {
+      console.log(AWS.CredentialProviderChain.defaultProviders)
       expect(
         AWS.CredentialProviderChain.defaultProviders.map((fn) => fn.name)
       ).to.eql([
-          'AWS.EnvironmentCredentials(\'AWS\')',
-          'AWS.EnvironmentCredentials(\'AMAZON\')',
-          'AWS.SharedIniFileCredentials()',
-          'AWS.ECSCredentials()',
-          'AWS.ProcessCredentials()',
-          'AWS.TokenFileWebIdentityCredentials()',
-          'AWS.EC2MetadataCredentials()'
+          'EnvironmentCredentialsAWS',
+          'EnvironmentCredentialsAMAZON',
+          'SharedIniFileCredentials',
+          'ECSCredentials',
+          'ProcessCredentials',
+          'TokenFileWebIdentityCredentials',
+          'EC2MetadataCredentials'
       ]);
     });
   });
