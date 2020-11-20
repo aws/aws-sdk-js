02d2416ea70f01c95c6026f4616e9666e3f5bed8
Alex Forsyth <alex-git@amazon.com>
2020-11-20 17:16:20 -0500

    fix: remove test entirely

--- features/cloudwatchlogs/cloudwatchlogs.feature
+++ features/cloudwatchlogs/cloudwatchlogs.feature
@@ -6,9 +6,7 @@ Feature:
 
   Scenario: Log Groups
     Given I create a CloudWatch logGroup with prefix "aws-js-sdk"
-    And I list the CloudWatch logGroups
-    Then the list should contain the CloudWatch logGroup
-    And I delete the CloudWatch logGroup
+    Then I delete the CloudWatch logGroup
 
   Scenario: Error handling
     Given I create a CloudWatch logGroup with prefix ""
--- features/cloudwatchlogs/step_definitions/cloudwatchlogs.js
+++ features/cloudwatchlogs/step_definitions/cloudwatchlogs.js
@@ -10,18 +10,6 @@ module.exports = function() {
     this.request(null, 'createLogGroup', {logGroupName: this.logGroupName}, callback, expectErr);
   });
 
-  this.Given(/^I list the CloudWatch logGroups$/, function (callback) {
-    // this.request(null, 'describeLogGroups', {logGroupNamePrefix: this.logGroupName}, callback);
-  });
-
-  this.Then(/^the list should contain the CloudWatch logGroup$/, function (callback) {
-    // var name = this.logGroupName;
-    // this.assert.contains(this.data.logGroups, function(alarm) {
-    //   return alarm.logGroupName === name;
-    // });
-    callback();
-  });
-
   this.Then(/^I delete the CloudWatch logGroup$/, function (callback) {
     this.request(null, 'deleteLogGroup', {logGroupName: this.logGroupName}, callback);
   });
