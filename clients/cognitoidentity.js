2f3789385b55589de0c6e05a0121ee7d1af49d4f
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-12-07 22:41:36 -0800

    chore: remove cognitoidentity customizations to disable auth (#3543)
    Commit which added authtype=none to respective APIs to allow unauthenticated requests
    https://github.com/aws/aws-sdk-js/commit/24bd2a1e85dc0c232fa9745abb7fa3f84741bd66#diff-d7c5211fba77e7558bc040639b1bdbd73447a5c99c31b4d9a83afa565a6618be

--- clients/cognitoidentity.js
+++ clients/cognitoidentity.js
@@ -5,7 +5,6 @@ var apiLoader = AWS.apiLoader;
 
 apiLoader.services['cognitoidentity'] = {};
 AWS.CognitoIdentity = Service.defineService('cognitoidentity', ['2014-06-30']);
-require('../lib/services/cognitoidentity');
 Object.defineProperty(apiLoader.services['cognitoidentity'], '2014-06-30', {
   get: function get() {
     var model = require('../apis/cognito-identity-2014-06-30.min.json');
