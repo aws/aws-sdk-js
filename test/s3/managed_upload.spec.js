aws/aws-sdk-js

commit e8f040e64dee32cc9a41f82e6e12b00cfa1258e6

fix using accesspoint in ManagedUpload (#3109)

lib/s3/managed_upload.js
@@ -284,7 +284,7 @@ AWS.S3.ManagedUpload = AWS.util.inherit({
       self.service = new AWS.S3({params: params});
     } else {
       var service = self.service;
-      var config = AWS.util.copy(service.config);
+      var config = AWS.util.copy(service._originalConfig || {});
       config.signatureVersion = service.getSignatureVersion();
       self.service = new service.constructor.__super__(config);
       self.service.config.params =

test/s3/managed_upload.spec.js
@@ -1176,7 +1176,7 @@
           });
         });
       }
-      return describe('tagging', function() {
+      describe('tagging', function() {
         it('should embed tags in PutObject request for single part uploads', function(done) {
           var reqs;
           reqs = helpers.mockResponses([
@@ -1366,6 +1366,52 @@
             e = error;
             return done();
           }
+        });
+      });
+
+      describe('accesspoint', function() {
+        it('should make subsequent calls with accesspoint', function(done) {
+          var reqs = helpers.mockResponses([
+            {
+              data: {
+                UploadId: 'uploadId'
+              }
+            }, {
+              data: {
+                ETag: 'ETAG1'
+              }
+            }, {
+              data: {
+                ETag: 'ETAG2'
+              }
+            }, {
+              data: {
+                ETag: 'FINAL_ETAG',
+                Location: 'FINAL_LOCATION'
+              }
+            }
+          ]);
+          var size = 18;
+          var opts = {
+            partSize: size,
+            queueSize: 1,
+            service: s3,
+            params: {
+              Body: bigbody,
+              Bucket: 'arn:aws:s3:us-west-2:123456789012:accesspoint/myendpoint'
+            }
+          };
+          var endpoint = 'myendpoint-123456789012.s3-accesspoint.us-west-2.amazonaws.com';
+          upload = new AWS.S3.ManagedUpload(opts);
+          return send({}, function() {
+            expect(helpers.operationsForRequests(reqs)).to.eql(['s3.createMultipartUpload', 's3.uploadPart', 's3.uploadPart', 's3.completeMultipartUpload']);
+            expect(err).not.to.exist;
+            expect(reqs[0].httpRequest.endpoint.hostname).to.equal(endpoint);
+            expect(reqs[1].httpRequest.endpoint.hostname).to.equal(endpoint);
+            expect(reqs[2].httpRequest.endpoint.hostname).to.equal(endpoint);
+            expect(reqs[3].httpRequest.endpoint.hostname).to.equal(endpoint);
+            return done();
+          });
         });
       });
     });

commit 6e3f8cca9f71b246af3f3924264c85b2cfcffd05

fix: managed upload not to resolve config repeatedly
Previously the managed upload creates new S3 clients with the
constructor parameters of the S3 client used to create the managed
upload. That results in that the newly created S3 client needs to
resolve every config, including the credentials over and over
again.

This issue was introduced by #3109 to make sure the internal S3
client inside of managed upload has the same constructor parameters
of the S3 client used to create the managed upload. So that the
logic relies on the constructor parameters like access point
and endpoint discovery works properly.

This change supply the resolved client config to create the
managed upload internal S3 client so configs don't need to be
resolved again, like credentials. Meanwhile, we explicitly set
the constructor parameter to be the same as that of S3 client
used to create the managed upload.

lib/s3/managed_upload.js
@@ -283,12 +283,18 @@ AWS.S3.ManagedUpload = AWS.util.inherit({
     if (!self.service) {
       self.service = new AWS.S3({params: params});
     } else {
+      // Create a new S3 client from the supplied client's constructor.
       var service = self.service;
-      var config = AWS.util.copy(service._originalConfig || {});
+      var config = AWS.util.copy(service.config);
       config.signatureVersion = service.getSignatureVersion();
       self.service = new service.constructor.__super__(config);
       self.service.config.params =
         AWS.util.merge(self.service.config.params || {}, params);
+      Object.defineProperty(self.service, '_originalConfig', {
+        get: function() { return service._originalConfig; },
+        enumerable: false,
+        configurable: true
+      });
     }
   },
 

scripts/changelog/change-creator.js
@@ -24,7 +24,7 @@ function generateRandomIdentifier() {
  * Ref: https://github.com/aws/aws-sdk-js/issues/3691
  */
 function sanitizeFileName(filename) {
-    return filename.replaceAll(/[^a-zA-Z0-9\\.]/g, '-');
+    return filename.replace(/[^a-zA-Z0-9\\.]/g, '-');
 }
 
 var CHANGES_DIR = path.join(process.cwd(), '.changes');

test/s3/managed_upload.spec.js
@@ -68,6 +68,7 @@
         }
       });
     };
+
     it('defaults to using sigv4', function() {
       var upload = new AWS.S3.ManagedUpload({
         params: {
@@ -76,6 +77,7 @@
       });
       expect(upload.service.getSignatureVersion()).to.eql('v4');
     });
+
     it('uses sigv4 if customer supplies a configured S3 client', function() {
       var upload = new AWS.S3.ManagedUpload({
         params: {
@@ -85,6 +87,7 @@
       });
       expect(upload.service.getSignatureVersion()).to.eql('v4');
     });
+
     it('uses sigv2 if customer supplies a configured S3 client', function() {
       var upload = new AWS.S3.ManagedUpload({
         params: {
@@ -94,7 +97,19 @@
       });
       expect(upload.service.getSignatureVersion()).to.eql('s3');
     });
-    return describe('send', function() {
+
+    it('uses resolved credential if customer supplies a configured S3 client', function() {
+      var credential = 'Some Credential Providers';
+      var upload = new AWS.S3.ManagedUpload({
+        params: {
+          Body: 'body'
+        },
+        service: new AWS.S3({credentials: credential })
+      });
+      expect(upload.service.config.credentials).to.be[credential];
+    });
+
+    describe('send', function() {
       it('default callback throws', function() {
         helpers.mockResponses([
           {