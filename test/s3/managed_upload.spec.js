345852e53a3a6599e76dd37ade4395e585700897
AllanZhengYP <zheallan@amazon.com>
2021-04-08 10:37:33 -0700

    fix: managed upload resolves config repeatedly (#3700)
    * fix: managed upload not to resolve config repeatedly
    
    Previously the managed upload creates new S3 clients with the
    constructor parameters of the S3 client used to create the managed
    upload. That results in that the newly created S3 client needs to
    resolve every config, including the credentials over and over
    again.
    
    This issue was introduced by #3109 to make sure the internal S3
    client inside of managed upload has the same constructor parameters
    of the S3 client used to create the managed upload. So that the
    logic relies on the constructor parameters like access point
    and endpoint discovery works properly.
    
    This change supply the resolved client config to create the
    managed upload internal S3 client so configs don't need to be
    resolved again, like credentials. Meanwhile, we explicitly set
    the constructor parameter to be the same as that of S3 client
    used to create the managed upload.
    
    * address feedbacks
    
    Co-authored-by: Trivikram Kamat <16024985+trivikr@users.noreply.github.com>

--- test/s3/managed_upload.spec.js
+++ test/s3/managed_upload.spec.js
@@ -68,6 +68,7 @@
         }
       });
     };
+
     it('defaults to using sigv4', function() {
       var upload = new AWS.S3.ManagedUpload({
         params: {
@@ -76,6 +77,7 @@
       });
       expect(upload.service.getSignatureVersion()).to.eql('v4');
     });
+
     it('uses sigv4 if customer supplies a configured S3 client', function() {
       var upload = new AWS.S3.ManagedUpload({
         params: {
@@ -85,6 +87,7 @@
       });
       expect(upload.service.getSignatureVersion()).to.eql('v4');
     });
+
     it('uses sigv2 if customer supplies a configured S3 client', function() {
       var upload = new AWS.S3.ManagedUpload({
         params: {
@@ -94,7 +97,19 @@
       });
       expect(upload.service.getSignatureVersion()).to.eql('s3');
     });
-    return describe('send', function() {
+
+    it('uses resolved credential if customer supplies a configured S3 client', function() {
+      var credential = 'Some Credential Providers';
+      var upload = new AWS.S3.ManagedUpload({
+        params: {
+          Body: 'body'
+        },
+        service: new AWS.S3({credentials: credential })
+      });
+      expect(upload.service.config.credentials).to.be[credential];
+    });
+
+    describe('send', function() {
       it('default callback throws', function() {
         helpers.mockResponses([
           {
