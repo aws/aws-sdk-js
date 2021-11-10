936bde053ba817da293fc1329b34b5f8354cb601
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-11-10 23:05:13 +0000

    chore: convert endpoint from module to function

--- test/metadata_service/get_metadata_service_endpoint.spec.js
+++ test/metadata_service/get_metadata_service_endpoint.spec.js
@@ -1,7 +1,7 @@
 var helpers = require('./../helpers');
 var getMetadataServiceEndpoint = require('../../lib/metadata_service/get_metadata_service_endpoint');
 
-var Endpoint = require('../../lib/metadata_service/endpoint');
+var Endpoint = require('../../lib/metadata_service/get_endpoint')();
 var EndpointMode = require('../../lib/metadata_service/endpoint_mode');
 var ENDPOINT_CONFIG_OPTIONS = require('../../lib/metadata_service/endpoint_config_options').ENDPOINT_CONFIG_OPTIONS;
 
