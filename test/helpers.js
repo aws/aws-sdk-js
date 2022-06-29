2a87ec1b21ffc9b705d10fa8b3e5128f451b926b
Ezhilan (Eddy) Ilangovan <44303762+eddy-aws@users.noreply.github.com>
2022-06-29 12:13:45 -0700

    Sqs AwsJson migration (#4105)
    * Add awsQueryCompatible trait support to service
    
    When awsQueryCompatible trait is found, it's made available as an API property. When this property is found,
    the error code is returned by looking up the mapping. This is a pre-requisite for migrating services from
    AWSQuery wire protocol to AWSJson.
    
    * Add additional tests; review comments
    
    * Add awsQueryCompatible trait support to service
    
    When awsQueryCompatible trait is found, it's made available as an API property. When this property is found,
    the error code is returned by looking up the mapping. This is a pre-requisite for migrating services from
    AWSQuery wire protocol to AWSJson.
    
    * Add additional tests; review comments
    
    * Review comments
    
    * Use MockServiceFromApi; Move errorCodeMapping logic to even)listeners
    
    * Update on.error function
    
    * Fix type
    
    * Remove reimplementation of mapping function
    
    * Use add instead of addAsync
    
    Co-authored-by: AllanZhengYP <zheallan@amazon.com>
    
    * Add null check to error listener
    
    * Use simple service null check
    
    * Fix add 'Error' implementation
    
    * Add changelog
    
    Co-authored-by: AllanZhengYP <zheallan@amazon.com>

--- test/helpers.js
+++ test/helpers.js
@@ -146,7 +146,7 @@
     return expect(results[0]).to.eql(results[1]);
   };
 
-  MockService = AWS.Service.defineService('mock', {
+  let mockServiceParams = {
     serviceIdentifier: 'mock',
     initialize: function(config) {
       AWS.Service.prototype.initialize.call(this, config);
@@ -166,7 +166,11 @@
           message: null
         };
       });
-    },
+    }
+  };
+
+  MockService = AWS.Service.defineService('mock', {
+    ...mockServiceParams,
     api: new AWS.Model.Api({
       metadata: {
         endpointPrefix: 'mockservice',
@@ -182,15 +186,7 @@
       customApi.metadata.signatureVersion = 'v4';
     }
     return AWS.Service.defineService('mock', {
-      serviceIdentifier: 'mock',
-      initialize: function(config) {
-        AWS.Service.prototype.initialize.call(this, config);
-        this.config.credentials = {
-          accessKeyId: 'akid',
-          secretAccessKey: 'secret'
-        };
-        this.config.region = this.config.region || 'mock-region';
-      },
+      ...mockServiceParams,
       api: new AWS.Model.Api(customApi)
     });
   };
