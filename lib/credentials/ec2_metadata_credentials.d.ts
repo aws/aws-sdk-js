48224d7e05a17199c259d9ab26d6f62e86bcc0f6
George Fu <kuhe@users.noreply.github.com>
2023-11-02 17:47:13 +0000

    undo formatting

--- lib/credentials/ec2_metadata_credentials.d.ts
+++ lib/credentials/ec2_metadata_credentials.d.ts
@@ -1,45 +1,45 @@
-import { Credentials } from "../credentials";
-import { Logger } from "../config-base";
+import {Credentials} from '../credentials';
+import {Logger} from '../config-base';
 
 export class EC2MetadataCredentials extends Credentials {
-  /**
-   * Creates credentials from the metadata service on an EC2 instance.
-   * @param {object} options - Override the default (1s) timeout period.
-   */
-  constructor(options?: EC2MetadataCredentialsOptions);
+    /**
+     * Creates credentials from the metadata service on an EC2 instance.
+     * @param {object} options - Override the default (1s) timeout period.
+     */
+    constructor(options?: EC2MetadataCredentialsOptions);
 
-  /**
-   * The original expiration of the current credential. In case of AWS
-   * outage, the EC2 metadata will extend expiration of the existing
-   * credential.
-   */
-  originalExpiration?: Date;
+    /**
+     * The original expiration of the current credential. In case of AWS
+     * outage, the EC2 metadata will extend expiration of the existing
+     * credential.
+     */
+    originalExpiration?: Date;
 }
 interface EC2MetadataCredentialsOptions {
-  httpOptions?: {
+    httpOptions?: {
+        /**
+         * Timeout in milliseconds.
+         */
+        timeout?: number
+        /**
+        * Connection timeout in milliseconds.
+        */
+        connectTimeout?: number
+    }
+    maxRetries?: number
+    logger?: Logger
     /**
-     * Timeout in milliseconds.
+     * Prevent IMDSv1 fallback.
      */
-    timeout?: number;
+    ec2MetadataV1Disabled?: boolean
+
     /**
-     * Connection timeout in milliseconds.
+     * profile name to check for IMDSv1 settings.
      */
-    connectTimeout?: number;
-  };
-  maxRetries?: number;
-  logger?: Logger;
-  /**
-   * Prevent IMDSv1 fallback.
-   */
-  ec2MetadataV1Disabled?: boolean;
+    profile?: string
 
-  /**
-   * profile name to check for IMDSv1 settings.
-   */
-  profile?: string;
-
-  /**
-   * optional file from which to to get config.
-   */
-  filename?: string;
+    /**
+     * optional file from which to to get config.
+     */
+    filename?: string
 }
