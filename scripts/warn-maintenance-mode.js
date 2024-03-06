703a1aa81779dec7d660548a2db99f22274f0b22
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-03-06 10:35:49 -0800

    Add upcoming end-of-support message in postinstall

---
+++ scripts/warn-maintenance-mode.js
@@ -0,0 +1,14 @@
+console.warn(
+  '\n╔═════════════════════════════════════════════════╗'
++ '\n║ The AWS SDK for JavaScript (v2) will reach      ║'
++ '\n║ -> maintenance mode on September 8, 2024.       ║'
++ '\n║ -> end-of-support on September 8, 2025.         ║'
++ '\n║                                                 ║'
++ '\n║ To continue receiving updates to AWS services,  ║'
++ '\n║ bug fixes, and security updates please upgrade  ║'
++ '\n║ to AWS SDK for JavaScript (v3).                 ║'
++ '\n║                                                 ║'
++ '\n║ More info: https://a.co/cUPnyil                 ║'
++ '\n╚═════════════════════════════════════════════════╝'
++ '\n'
+);
