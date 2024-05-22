3fef5d0e0b2707db975921a39b98329d4f2d1bb3
Yifei Chen <fennu637@sina.com>
2024-05-23 05:01:30 +0800

    fix TokenFileWebIdentityCredentials typing (#4388)

--- lib/credentials/token_file_web_identity_credentials.d.ts
+++ lib/credentials/token_file_web_identity_credentials.d.ts
@@ -1,12 +1,12 @@
 import {Credentials} from '../credentials';
 import {AWSError} from '../error';
-import {ConfigurationOptions} from '../config-base';
+import {ClientConfiguration} from '../../clients/sts';
 export class TokenFileWebIdentityCredentials extends Credentials {
     /**
      * Creates a new credentials object with optional configuraion.
      * @param {Object} clientConfig - a map of configuration options to pass to the underlying STS client.
      */
-    constructor(clientConfig?: ConfigurationOptions);
+    constructor(clientConfig?: ClientConfiguration);
     /**
      * Refreshes credentials using AWS.STS.assumeRoleWithWebIdentity().
      */
